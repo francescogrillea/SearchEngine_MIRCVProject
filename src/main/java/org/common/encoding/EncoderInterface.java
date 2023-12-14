a6a4f37781623efea05388bee91f7bb90d31816c
francescogrillea <grilleafrancesco@gmail.com>
2023-12-14 11:15:18 +0100

    encoding list of values

--- src/main/java/org/common/encoding/EncoderInterface.java
+++ src/main/java/org/common/encoding/EncoderInterface.java
@@ -1,10 +1,13 @@
 package org.common.encoding;
 
 import java.nio.ByteBuffer;
+import java.util.List;
 
 public interface EncoderInterface {
 
+    ByteBuffer encodeList(List<Integer> list);
     byte[] encode(int value);
+    List<Integer> decodeList(ByteBuffer encodedBytes);
     int decode(ByteBuffer encodedBytes);
 
 }
