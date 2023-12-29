8c06eefbc27872141085dd1d673fa5385b4be58d
GiovanniBergami <g.bergami@studenti.unipi.it>
2023-12-29 11:36:30 +0100

    postinglist block reader test added

--- src/test/java/org/common/PostingListBlockReaderTest.java
+++ src/test/java/org/common/PostingListBlockReaderTest.java
@@ -2,7 +2,7 @@ package org.common;
 import org.common.encoding.NoEncoder;
 import org.common.encoding.UnaryEncoder;
 import org.common.encoding.VBEncoder;
-import org.junit.Assert;
+
 import org.junit.Test;
 
 import java.io.FileInputStream;
@@ -27,10 +27,44 @@ public class PostingListBlockReaderTest {
             System.out.println(p);
             pb.readBlock();
             System.out.println(pb.getDocID());
+
+            for(int i = 0; i< p.getSize();i++){ //check sequentially
+                assertEquals(pb.getDocID(),p.getDocId(i));
+                assertEquals(pb.getTermFreq(),p.getTermFrequency(i));
+                pb.nextPosting();
+            }
+
+
+        }catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch(IOException e){
+            System.out.println(e.getMessage()); //TODO: mettere try with resuorces. capire perchè non va sia qui che sotto
+        }
+    }
+
+    @Test
+    public void nextGEQ(){
+        try {
+
+            FileChannel fc = (new FileInputStream("data/index.bin")).getChannel();
+            Lexicon lexicon = LexiconReader.readLexicon("data/lexicon.bin");
+            TermEntry t = lexicon.get("manhattan").getTermEntryList().get(0);
+            PostingListBlockReader pb = new PostingListBlockReader(t, "manhattan", true);
+            PostingListReader.setEncoder(new NoEncoder(), new NoEncoder());
+            PostingList p = PostingListReader.readPostingList(t);
+            pb.readBlock();
+
+            int[] indexes = new int[]{1,4,8};
+            for(int i=0; i<indexes.length; i++){
+                int docId= p.getDocId(i);
+                assertEquals((int)pb.nextGEQ(docId),p.getTermFrequency(indexes[i]));
+            }
+
         }catch(FileNotFoundException e){
             System.out.println(e.getMessage());
         }catch(IOException e){
             System.out.println(e.getMessage()); //TODO: mettere try with resuorces. capire perchè non va
         }
     }
+
 }
